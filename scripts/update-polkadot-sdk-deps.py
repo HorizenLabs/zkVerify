0f1bab16ba9af4c5ae9a2d8050ed3ba7842fc147
JackPiri <gpirinoli@horizenlabs.io>
2024-07-03 16:26:08 +0200

    From toml Python library to tomlkit Python library
    This makes toml file writing more relibale and easier

--- scripts/update-polkadot-sdk-deps.py
+++ scripts/update-polkadot-sdk-deps.py
@@ -6,11 +6,11 @@ Usage example:
 """
 
 import argparse
-from git import Repo
+import git
 import tempfile
-import toml # pip install toml
 import os
 import subprocess
+import tomlkit # pip install tomlkit
 
 def parse_arguments():
     """
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         git_url = "git@github.com:paritytech/polkadot-sdk.git"
         print(f"Temporary cloning Polkadot-SDK branch {args.branch} into {tmp_dir}...")
         try:
-            repo_polkadot = Repo.clone_from(git_url, tmp_dir, branch=args.branch, depth=1)
+            repo_polkadot = git.Repo.clone_from(git_url, tmp_dir, branch=args.branch, depth=1)
         except Exception as e:
             print(f"...error cloning! The script will exit now!")
             exit(-1)
@@ -51,11 +51,11 @@ if __name__ == "__main__":
         workspace_file_path_polkadot = f"{tmp_dir}/Cargo.toml"
         print_verbose(args.verbose, "Polkadot-SDK libraries:")
         with open(workspace_file_path_polkadot) as workspace_file_polkadot:
-            workspace_toml = toml.load(workspace_file_polkadot)
-            for library_path in workspace_toml["workspace"]["members"]:
+            workspace_toml_polkadot = tomlkit.load(workspace_file_polkadot)
+            for library_path in workspace_toml_polkadot["workspace"]["members"]:
                 library_file = f"{tmp_dir}/{library_path}/Cargo.toml"
                 with open(library_file) as library_file:
-                    library_toml = toml.load(library_file)
+                    library_toml = tomlkit.load(library_file)
                     polkadot_libs[library_toml['package']['name']] = library_toml['package']['version']
                     print_verbose(args.verbose, f"  Name: {library_toml['package']['name']} - Version: {library_toml['package']['version']}")
 
@@ -63,48 +63,31 @@ if __name__ == "__main__":
         workspace_file_path_zkverify = f"{os.getcwd()}/Cargo.toml"
         zkverify_deps = {}
         with open(workspace_file_path_zkverify) as workspace_file_zkverify:
-            workspace_toml = toml.load(workspace_file_zkverify)
-            for library_name in workspace_toml["workspace"]["dependencies"]:
+            workspace_toml_zkverify = tomlkit.load(workspace_file_zkverify)
+            for library_name in workspace_toml_zkverify["workspace"]["dependencies"]:
                 if (library_name in polkadot_libs):
-                    library_info = workspace_toml["workspace"]["dependencies"][library_name]
-                    if ("version" in library_info):
+                    library_info = workspace_toml_zkverify["workspace"]["dependencies"][library_name]
+                    if (type(library_info) is tomlkit.items.InlineTable and
+                        "version" in library_info):
                         # e.g.: my_lib = { version = "0.1.0", default-features = false }
-                        version = str(library_info["version"])
-                        version_updated = workspace_toml["workspace"]["dependencies"][library_name]
-                        version_updated["version"] = polkadot_libs[library_name]
-                        version_updated = "{ " + toml.dumps(version_updated).replace("\n", ", ")[0:-2] + " }"
-                    elif str(library_info).count("{") == 0:
+                        version = library_info["version"]
+                        library_info["version"] = polkadot_libs[library_name]
+                    elif (type(library_info) is tomlkit.items.String):
                         # e.g.: my_lib = "0.1.0"
-                        version = str(library_info)
-                        version_updated = f"\"{polkadot_libs[library_name]}\""
+                        version = library_info
+                        workspace_toml_zkverify["workspace"]["dependencies"][library_name] = polkadot_libs[library_name]
                     else:
                         print(f"WARNING: unable to determine library version for {library_name}!")
                     if version != polkadot_libs[library_name]:
                         print(f"{library_name} is going to be updated (from {version} to {polkadot_libs[library_name]})")
-                        zkverify_deps[library_name] = version_updated
                     else:
                         print_verbose(args.verbose, f"{library_name} is not going to be updated")
         print(f"...checking done!")
 
         print(f"Updating Cargo.toml file...")
         workspace_file_path = f"{os.getcwd()}/Cargo.toml"
-        with open(workspace_file_path, 'r') as workspace_file:
-            read_lines = workspace_file.readlines()
-        lines_to_write = []
-        multi_line_lib = False
-        for read_line in read_lines:
-            library_name = read_line.split("=")[0].strip()
-            if (library_name in zkverify_deps):
-                lines_to_write.append(f"{library_name} = {zkverify_deps[library_name]}\n")
-                if read_line.count("{") == 1 and read_line.count("}") == 0:
-                    multi_line_lib = True
-            else:
-                if  (not multi_line_lib):
-                    lines_to_write.append(read_line)
-                if read_line.count("{") == 0 and read_line.count("}") == 1:
-                    multi_line_lib = False
-        with open(workspace_file_path, 'w') as workspace_file:
-            lines = workspace_file.writelines(lines_to_write)
+        with open(workspace_file_path_zkverify, 'w') as workspace_file_zkverify:
+            workspace_file_zkverify.writelines(tomlkit.dumps(workspace_toml_zkverify))
         print(f"...updating done!")
 
         if (not args.no_check):
@@ -120,7 +103,7 @@ if __name__ == "__main__":
             print(f"...checking done!")
 
         if (not args.no_commit):
-            repo_zkverify = Repo(os.getcwd())
+            repo_zkverify = git.Repo(os.getcwd())
             print(f"Committing to zkVerify repository...")
             commit_message = f"Bump dependencies to Polkadot-SDK branch {args.branch} (commit {commit_hash})"
             print(f"Using commit message: {commit_message}.")
