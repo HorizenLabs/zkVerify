3674756e66209f5c4f807af8e298ac8b658be2bc
la10736 <michele.damico@gmail.com>
2024-07-09 16:21:54 +0200

    First eaw attempt

---
+++ relay-node/service/src/grandpa_support.rs
@@ -0,0 +1,58 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Polkadot-specific GRANDPA integration utilities.
+
+use sp_runtime::traits::{Block as BlockT, Header as _, NumberFor};
+
+use crate::HeaderProvider;
+
+#[cfg(feature = "full-node")]
+use polkadot_primitives::{Block, Hash};
+
+/// Returns the block hash of the block at the given `target_number` by walking
+/// backwards from the given `current_header`.
+pub(super) fn walk_backwards_to_target_block<Block, HP>(
+    backend: &HP,
+    target_number: NumberFor<Block>,
+    current_header: &Block::Header,
+) -> Result<(Block::Hash, NumberFor<Block>), sp_blockchain::Error>
+where
+    Block: BlockT,
+    HP: HeaderProvider<Block>,
+{
+    let mut target_hash = current_header.hash();
+    let mut target_header = current_header.clone();
+
+    loop {
+        if *target_header.number() < target_number {
+            unreachable!(
+                "we are traversing backwards from a known block; \
+				 blocks are stored contiguously; \
+				 qed"
+            );
+        }
+
+        if *target_header.number() == target_number {
+            return Ok((target_hash, target_number));
+        }
+
+        target_hash = *target_header.parent_hash();
+        target_header = backend
+            .header(target_hash)?
+            .expect("Header known to exist due to the existence of one of its descendants; qed");
+    }
+}
---
+++ relay-node/service/src/rpc.rs
@@ -0,0 +1,113 @@
+use consensus_common::SelectChain;
+use jsonrpsee::RpcModule;
+use polkadot_primitives::{AccountId, Balance, Block, BlockNumber, Nonce};
+use polkadot_rpc::{BabeDeps, FullDeps, GrandpaDeps, RpcExtension};
+use sc_client_api::AuxStore;
+use sp_api::ProvideRuntimeApi;
+use sp_block_builder::BlockBuilder;
+use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
+use sp_consensus_babe::BabeApi;
+use substrate_frame_rpc_system as frame_rpc_system;
+
+/// Instantiate all RPC extensions.
+pub fn create_full<C, P, SC, B>(
+    FullDeps {
+        client,
+        pool,
+        select_chain,
+        chain_spec: _,
+        deny_unsafe,
+        babe,
+        grandpa,
+        beefy,
+        backend,
+    }: FullDeps<C, P, SC, B>,
+) -> Result<RpcExtension, Box<dyn std::error::Error + Send + Sync>>
+where
+    C: ProvideRuntimeApi<Block>
+        + HeaderBackend<Block>
+        + AuxStore
+        + HeaderMetadata<Block, Error = BlockChainError>
+        + Send
+        + Sync
+        + 'static,
+    C::Api: frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+    C::Api: mmr_rpc::MmrRuntimeApi<Block, <Block as sp_runtime::traits::Block>::Hash, BlockNumber>,
+    C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+    C::Api: BabeApi<Block>,
+    C::Api: BlockBuilder<Block>,
+    P: sc_transaction_pool_api::TransactionPool + Sync + Send + 'static,
+    SC: SelectChain<Block> + 'static,
+    B: sc_client_api::Backend<Block> + Send + Sync + 'static,
+    B::State: sc_client_api::StateBackend<sp_runtime::traits::HashingFor<Block>>,
+{
+    use frame_rpc_system::{System, SystemApiServer};
+    use mmr_rpc::{Mmr, MmrApiServer};
+    use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
+    use sc_consensus_babe_rpc::{Babe, BabeApiServer};
+    use sc_consensus_beefy_rpc::{Beefy, BeefyApiServer};
+    use sc_consensus_grandpa_rpc::{Grandpa, GrandpaApiServer};
+    use substrate_state_trie_migration_rpc::{StateMigration, StateMigrationApiServer};
+
+    let mut io = RpcModule::new(());
+    let BabeDeps {
+        babe_worker_handle,
+        keystore,
+    } = babe;
+    let GrandpaDeps {
+        shared_voter_state,
+        shared_authority_set,
+        justification_stream,
+        subscription_executor,
+        finality_provider,
+    } = grandpa;
+
+    io.merge(StateMigration::new(client.clone(), backend.clone(), deny_unsafe).into_rpc())?;
+    io.merge(System::new(client.clone(), pool.clone(), deny_unsafe).into_rpc())?;
+    io.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+    io.merge(
+        Mmr::new(
+            client.clone(),
+            backend
+                .offchain_storage()
+                .ok_or("Backend doesn't provide the required offchain storage")?,
+        )
+        .into_rpc(),
+    )?;
+    io.merge(
+        Babe::new(
+            client.clone(),
+            babe_worker_handle.clone(),
+            keystore,
+            select_chain,
+            deny_unsafe,
+        )
+        .into_rpc(),
+    )?;
+    io.merge(
+        Grandpa::new(
+            subscription_executor,
+            shared_authority_set.clone(),
+            shared_voter_state,
+            justification_stream,
+            finality_provider,
+        )
+        .into_rpc(),
+    )?;
+    // What we removed from polkadot_rpc::create_full();
+    // io.merge(
+    //     SyncState::new(chain_spec, client, shared_authority_set, babe_worker_handle)?
+    //         .into_rpc(),
+    // )?;
+
+    io.merge(
+        Beefy::<Block>::new(
+            beefy.beefy_finality_proof_stream,
+            beefy.beefy_best_block_stream,
+            beefy.subscription_executor,
+        )?
+        .into_rpc(),
+    )?;
+
+    Ok(io)
+}
