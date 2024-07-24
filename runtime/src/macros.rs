c398cfccd2bf3eb01345d58c4cce476603548039
la10736 <michele.damico@gmail.com>
2024-07-24 14:01:26 +0200

    Some small refactor

---
+++ runtime/src/macros.rs
@@ -0,0 +1,20 @@
+macro_rules! prod_or_fast {
+    ($prod:expr, $test:expr) => {
+        if cfg!(feature = "fast-runtime") {
+            $test
+        } else {
+            $prod
+        }
+    };
+    ($prod:expr, $test:expr, $env:expr) => {
+        if cfg!(feature = "fast-runtime") {
+            core::option_env!($env)
+                .map(|s| s.parse().ok())
+                .flatten()
+                .unwrap_or($test)
+        } else {
+            $prod
+        }
+    };
+}
+pub(crate) use prod_or_fast;
