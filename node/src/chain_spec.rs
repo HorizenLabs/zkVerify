692a34ea4e0f3698ec0058c06b948ebd2196d311
Rodrigo Doria Medina <rodrigo@horizenlabs.io>
2024-07-24 12:38:31 -0600

    feat: SS58 prefix change

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -126,6 +126,7 @@ pub fn development_config() -> Result<ChainSpec, String> {
         let mut props = Properties::new();
         props.insert("tokenSymbol".into(), "ACME".into());
         props.insert("tokenDecimals".into(), 18.into());
+        props.insert("ss58Format".into(), nh_runtime::SS58Prefix::get().into());
         props
     })
     .with_genesis_config_patch(genesis(
@@ -161,6 +162,7 @@ pub fn local_config() -> Result<ChainSpec, String> {
         let mut props = Properties::new();
         props.insert("tokenSymbol".into(), "ACME".into());
         props.insert("tokenDecimals".into(), 18.into());
+        props.insert("ss58Format".into(), nh_runtime::SS58Prefix::get().into());
         props
     })
     .with_genesis_config_patch(genesis(
--- scripts/snapshot-restore-balance/snapshot-balances.js
+++ scripts/snapshot-restore-balance/snapshot-balances.js
@@ -1,19 +1,26 @@
 const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 const fs = require('fs');
+const { decodeAddress, encodeAddress } = require('@polkadot/util-crypto');
+const { u8aToHex } = require('@polkadot/util');
 
 const WS_ENDPOINT = 'wss://testnet-rpc.zkverify.io';
-const DISCARDED_ACCOUNTS = [
-        "5D9txxK9DTvgCznTjJo7q1cxAgmWa83CzHvcz8zhBtLgaLBV",
-        "5ETuZEyLnfVzQCaDM8aQCcsNnz6xjPKvQCtqynCLqwng8QLd",
-        "5D29UEzgStCBTnjKNdkurDNvd3FHePHgTkPEUvjXYvg3brJj",
-        "5DiMVAp8WmFyWAwaTwAr7sU4K3brXcgNCBDbHoBWj3M46PiP",
-        "5EhREncHsntgJaax9YQphk1xN3LxPu2Rzbz4A3g7Ut8cRXWq",
-        "5C84NU2477uHCUF1A8rHb89sP2D2ZsnquPaGa2Htv75FN9gm",
-        "5HdZjrmNAkWQhYQUPNv7YRYnT4vyQswjbNm8eXBvULNQz5wH",
-        "5HjFLKpiCStQgRm6ZM1fT1R5pLKAqQdUG3uh7pvzaQfhdFuB",
-        "5ECktCamcAtBFJirEzvvJmXFxgLMCTAejhqZwLT1Dxn2fwB1",
-        "5EZbvFqx3j7ejqBSPWseif8xL3PwoqMQHdMT8rs9qWoHcdR3"
+const DISCARDED_ACCOUNTS_PUBLIC_KEYS = [
+        "0x3031dac6e5d70594a193a32fbfd9424953861ef4f348c6150d2576bab660ce7b",
+        "0x6a2a7ecf13da1ee5dce50684fd2e4059ccaca63c9b3354e076fead78b648b069",
+        "0x2a48b82b0a451b221f21d310a30229eba4aea938e826a229af08b5cc91577132",
+        "0x48f2fc2b628c75d8f297b558725f262f13e072d984c61f6a4a1491db363ad432",
+        "0x74789a7fb9b37cc6e74cf9599d829a0d8d99b4d9c7bc33388ffda50715218d57",
+        "0x028ecaab2910ec4b076999d8af270c7d46b46d493cce8ecf6bdda9cf8a68b66c",
+        "0xf63cf0a167de0d342750a7187a961407ad7d74cb67ce93631e93636646a15c69",
+        "0xfa9273ff2780b6b2923670792ad5899e9c72ac0dc59526160974f61231177e05",
+        "0x5e9c9e3190de7f188613ac26948cd2708b35866704e6bcd5f4180d0275a6d151",
+        "0x6e8298157ef6835a12a28c7b13e82e5bc3d327a88a4cffbc5c810223f3a2a91c"
     ]
+
+function ss58ToPublicKey(ss58Address) {
+    const publicKeyU8a = decodeAddress(ss58Address);
+    return u8aToHex(publicKeyU8a);
+}
 
 async function takeSnapshot(api, snapshotBalancesFile) {
     const balances = {};
@@ -21,15 +28,16 @@ async function takeSnapshot(api, snapshotBalancesFile) {
 
     for (const key of accounts) {
         const account = key.args[0].toString();
-        if (DISCARDED_ACCOUNTS.includes(account)) continue;
-        const { data:  { free: freeBalance, reserved: reservedBalance } } = await api.query.system.account(account);
+        const account_public_key = ss58ToPublicKey(account);
+        if (DISCARDED_ACCOUNTS_PUBLIC_KEYS.includes(account_public_key)) continue;
+        const { data:  { free: freeBalance, reserved: reservedBalance } } = await api.query.system.account(account_public_key);
         const totalBalance = BigInt(freeBalance.toString()) + BigInt(reservedBalance.toString());
-        balances[account] = totalBalance.toString();
+        balances[account_public_key] = totalBalance.toString();
     }
 
     const sortedBalances = Object.entries(balances).sort((a, b) => (BigInt(b[1]) < BigInt(a[1]) ? 1 : -1));
-    const sortedBalancesObj = sortedBalances.reduce((acc, [account, balance]) => {
-        acc[account] = balance;
+    const sortedBalancesObj = sortedBalances.reduce((acc, [account_public_key, balance]) => {
+        acc[account_public_key] = balance;
         return acc;
     }, {});
 
