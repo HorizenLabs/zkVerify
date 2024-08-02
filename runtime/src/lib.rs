b160f6d4636679e7704c64892b7c3583e99081b7
JackPiri <gpirinoli@horizenlabs.io>
2024-08-02 15:43:18 +0200

    Bump node and runtime version to 0.5.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // The version of the runtime specification. A full node will not attempt to use its native
     //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    spec_version: 4_000,
+    spec_version: 5_000,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
