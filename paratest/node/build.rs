0c4c20c330cf501cc48cd834efd0735bbdd38e3f
Daniele Rogora <danielerogora@horizenlabs.io>
2024-08-07 15:49:46 +0200

    Use config.toml for ldd

--- paratest/node/build.rs
+++ paratest/node/build.rs
@@ -1,7 +1,6 @@
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
-    println!("cargo:rustc-link-arg=-fuse-ld=lld");
     generate_cargo_keys();
 
     rerun_if_git_head_changed();
