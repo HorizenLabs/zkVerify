9003a6da48865927b504ac5a044004d98b7361d6
Daniele Rogora <danielerogora@horizenlabs.io>
2024-08-09 11:32:49 +0200

    Fix e2e CI

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -80,8 +80,7 @@ pub use sp_runtime::{Perbill, Permill};
 
 pub mod governance;
 use governance::pallet_custom_origins;
-// #[cfg(feature = "relay")]
-// use parachains::*;
+
 pub mod macros {
     macro_rules! prod_or_fast {
         ($prod:expr, $test:expr) => {
