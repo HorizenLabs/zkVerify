b09047d8dcccb2c0178d565565ceaa0d8bc7aa0f
Daniele Rogora <danielerogora@horizenlabs.io>
2024-08-13 11:07:24 +0200

    More clippy

--- relay-node/service/src/chain_spec.rs
+++ relay-node/service/src/chain_spec.rs
@@ -12,6 +12,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #![allow(clippy::type_complexity)]
 
 use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
