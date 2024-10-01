5dff0be188153e90903c6ab040d32564afcfb789
Daniele Rogora <danielerogora@horizenlabs.io>
2024-10-01 11:39:19 +0200

    Configure XCM for basic functionality and tests

--- runtime/src/weights.rs
+++ runtime/src/weights.rs
@@ -48,6 +48,10 @@ pub mod pallet_ultraplonk_verifier;
 pub mod pallet_utility;
 pub mod pallet_vesting;
 pub mod pallet_whitelist;
+#[cfg(feature = "relay")]
+pub mod pallet_xcm;
 pub mod pallet_zksync_verifier;
+#[cfg(feature = "relay")]
+pub mod xcm;
 
 pub mod parachains;
