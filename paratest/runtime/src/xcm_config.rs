2c4010ac1e57c0585dbe9571cc7cab7baaad5796
Daniele Rogora <danielerogora@horizenlabs.io>
2024-10-14 10:02:31 +0200

    Xcm benchmarks gating and more cleanups

--- paratest/runtime/src/xcm_config.rs
+++ paratest/runtime/src/xcm_config.rs
@@ -23,11 +23,10 @@ use parachains_common::xcm_config::ConcreteAssetFromSystem;
 use polkadot_runtime_common::impls::ToAuthor;
 use xcm::latest::prelude::*;
 use xcm_builder::{
-    AccountId32Aliases, AllowUnpaidExecutionFrom, ChildParachainConvertsVia, EnsureXcmOrigin,
-    FixedWeightBounds, FrameTransactionalProcessor, FungibleAdapter, IsConcrete, MintLocation,
-    NativeAsset, ParentIsPreset, RelayChainAsNative, SiblingParachainAsNative,
-    SignedAccountId32AsNative, SignedToAccountId32, SovereignSignedViaLocation, UsingComponents,
-    WithUniqueTopic,
+    AccountId32Aliases, AllowUnpaidExecutionFrom, EnsureXcmOrigin, FixedWeightBounds,
+    FrameTransactionalProcessor, FungibleAdapter, IsConcrete, MintLocation, NativeAsset,
+    ParentIsPreset, RelayChainAsNative, SiblingParachainAsNative, SignedAccountId32AsNative,
+    SignedToAccountId32, SovereignSignedViaLocation, UsingComponents, WithUniqueTopic,
 };
 use xcm_executor::XcmExecutor;
 
@@ -50,7 +49,6 @@ pub type LocationToAccountId = (
     //SiblingParachainConvertsVia<Sibling, AccountId>,
     //// Straight up local `AccountId32` origins just alias directly to `AccountId`.
     AccountId32Aliases<RelayNetwork, AccountId>,
-    ChildParachainConvertsVia<ParaId, AccountId>,
 );
 
 /// Means for transacting assets on this chain.
