f3119adf5c11b313cd6e0d843902702145dbcf91
Daniele Rogora <danielerogora@horizenlabs.io>
2024-11-14 17:42:42 +0100

    Disable clippy and fmt for crates in 'patches'

--- patches/cumulus/client/relay-chain-inprocess-interface/src/lib.rs
+++ patches/cumulus/client/relay-chain-inprocess-interface/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(clippy::all)]
+
 use std::{collections::btree_map::BTreeMap, pin::Pin, sync::Arc, time::Duration};
 
 use async_trait::async_trait;
