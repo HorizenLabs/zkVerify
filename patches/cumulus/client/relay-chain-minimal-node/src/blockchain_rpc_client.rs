b3a982f5653dfc51a3771c3913f85d1e49ed04e9
Daniele Rogora <danielerogora@horizenlabs.io>
2024-11-14 17:42:42 +0100

    Disable clippy and fmt for crates in 'patches'

--- patches/cumulus/client/relay-chain-minimal-node/src/blockchain_rpc_client.rs
+++ patches/cumulus/client/relay-chain-minimal-node/src/blockchain_rpc_client.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+#![rustfmt::skip]
+
 use std::{
 	collections::{BTreeMap, VecDeque},
 	pin::Pin,
