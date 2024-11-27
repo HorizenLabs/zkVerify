57d03bcc3b75950da24795043499f2ac59c40a90
Nikita Masych <nikitamasich152@gmail.com>
2024-11-27 13:57:05 +0200

    fix: using stable toolchain, updated benches and tests in plonky2 to pass

--- verifiers/plonky2/src/benchmarking.rs
+++ verifiers/plonky2/src/benchmarking.rs
@@ -5,6 +5,7 @@ use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
 use hp_verifiers::Verifier;
 use pallet_verifiers::{VkOrHash, Vks};
+use pallet_aggregate::{funded_account, insert_domain};
 
 pub struct Pallet<T: Config>(crate::Pallet<T>);
 pub trait Config: pallet_verifiers::Config<Plonky2> {}
@@ -13,15 +14,21 @@ pub type Call<T> = pallet_verifiers::Call<T, Plonky2>;
 
 include!("resources.rs");
 
-#[benchmarks]
+fn init<T: pallet_aggregate::Config>() -> (T::AccountId, u32) {
+    let caller: T::AccountId = funded_account::<T>();
+    let domain_id = 1;
+    insert_domain::<T>(domain_id, caller.clone(), Some(1));
+    (caller, domain_id)
+}
+
+#[benchmarks(where T: pallet_aggregate::Config)]
 mod benchmarks {
 
     use super::*;
 
     #[benchmark]
     fn submit_proof() {
-        // setup code
-        let caller = whitelisted_caller();
+        let (caller, domain_id) = init::<T>();
 
         let TestData { vk, proof, pubs } = get_valid_test_data();
 
@@ -31,13 +38,15 @@ mod benchmarks {
             VkOrHash::from_vk(vk),
             proof.into(),
             pubs.into(),
+            Some(domain_id),
         );
     }
 
     #[benchmark]
     fn submit_proof_with_vk_hash() {
         // setup code
-        let caller = whitelisted_caller();
+        let (caller, domain_id) = init::<T>();
+
         let TestData { vk, proof, pubs } = get_valid_test_data();
         let hash = sp_core::H256::repeat_byte(2);
         Vks::<T, Plonky2>::insert(hash, vk);
@@ -48,6 +57,7 @@ mod benchmarks {
             VkOrHash::from_hash(hash),
             proof.into(),
             pubs.into(),
+            Some(domain_id),
         );
     }
 
@@ -69,8 +79,9 @@ mod benchmarks {
 
 #[cfg(test)]
 mod mock {
-    use frame_support::derive_impl;
+    use frame_support::{traits::EnsureOrigin, derive_impl};
     use sp_runtime::{traits::IdentityLookup, BuildStorage};
+    use sp_core::{ConstU32, ConstU64};
 
     // Configure a mock runtime to test the pallet.
     frame_support::construct_runtime!(
@@ -78,14 +89,63 @@ mod mock {
         {
             System: frame_system,
             VerifierPallet: crate,
+            Balances: pallet_balances,
+            Aggregate: pallet_aggregate,
         }
     );
+
+    type Balance = u64;
 
     #[derive_impl(frame_system::config_preludes::SolochainDefaultConfig as frame_system::DefaultConfig)]
     impl frame_system::Config for Test {
         type Block = frame_system::mocking::MockBlockU32<Test>;
         type AccountId = u64;
+        type AccountData = pallet_balances::AccountData<Balance>;
         type Lookup = IdentityLookup<Self::AccountId>;
+    }
+
+    pub struct NoManager;
+    impl EnsureOrigin<RuntimeOrigin> for NoManager {
+        type Success = ();
+
+        fn try_origin(o: RuntimeOrigin) -> Result<Self::Success, RuntimeOrigin> {
+            Err(o)
+        }
+
+        fn try_successful_origin() -> Result<RuntimeOrigin, ()> {
+            Err(())
+        }
+    }
+
+    impl pallet_aggregate::Config for Test {
+        type RuntimeEvent = RuntimeEvent;
+        type RuntimeHoldReason = RuntimeHoldReason;
+        type AggregationSize = ConstU32<32>;
+        type MaxPendingPublishQueueSize = ConstU32<16>;
+        type ManagerOrigin = NoManager;
+        type Hold = Balances;
+        type Consideration = ();
+        type EstimateCallFee = ConstU32<1_000_000>;
+        type ComputePublisherTip = ();
+        type WeightInfo = ();
+        const AGGREGATION_SIZE: u32 = 32;
+        type Currency = Balances;
+    }
+
+    impl pallet_balances::Config for Test {
+        type RuntimeEvent = RuntimeEvent;
+        type RuntimeHoldReason = RuntimeHoldReason;
+        type RuntimeFreezeReason = RuntimeFreezeReason;
+        type WeightInfo = ();
+        type Balance = Balance;
+        type DustRemoval = ();
+        type ExistentialDeposit = ConstU64<1>;
+        type AccountStore = System;
+        type ReserveIdentifier = [u8; 8];
+        type FreezeIdentifier = RuntimeFreezeReason;
+        type MaxLocks = ConstU32<10>;
+        type MaxReserves = ConstU32<10>;
+        type MaxFreezes = ConstU32<10>;
     }
 
     impl pallet_verifiers::Config<crate::Plonky2> for Test {
